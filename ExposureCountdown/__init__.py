3413ad963593997f2098eb12855a1290eda8dded
Jen <65942744+jenerative@users.noreply.github.com>
2024-12-26 12:53:28 -0600

    fixed init maybe

--- ExposureCountdown/__init__.py
+++ ExposureCountdown/__init__.py
@@ -1,29 +1,36 @@
 # This file initializes the ExposureCountdown cog. It may contain setup functions to load the cog into the Red Discord Bot.
 
-from redbot.core import commands
+# from redbot.core import commands
 
-class ExposureCountdown(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+# class ExposureCountdown(commands.Cog):
+#     def __init__(self, bot):
+#         self.bot = bot
 
-    @commands.command()
-    async def upload_file(self, ctx, file: str):
-        # Logic for uploading a file
-        pass
+#     @commands.command()
+#     async def upload_file(self, ctx, file: str):
+#         # Logic for uploading a file
+#         pass
 
-    @commands.command()
-    async def set_privacy_duration(self, ctx, duration: int):
-        # Logic for setting privacy duration
-        pass
+#     @commands.command()
+#     async def set_privacy_duration(self, ctx, duration: int):
+#         # Logic for setting privacy duration
+#         pass
 
-    @commands.command()
-    async def extend_privacy_duration(self, ctx, additional_time: int):
-        # Logic for extending privacy duration
-        pass
+#     @commands.command()
+#     async def extend_privacy_duration(self, ctx, additional_time: int):
+#         # Logic for extending privacy duration
+#         pass
 
-    async def check_expiry(self):
-        # Logic for checking if the file privacy has expired
-        pass
+#     async def check_expiry(self):
+#         # Logic for checking if the file privacy has expired
+#         pass
 
-def setup(bot):
-    bot.add_cog(ExposureCountdown(bot))
+# def setup(bot):
+#     bot.add_cog(ExposureCountdown(bot))
+
+# This file initializes the ExposureCountdown cog. It may contain setup functions to load the cog into the Red Discord Bot.
+
+from .exposure_countdown import ExposureCountdown
+
+async def setup(bot):
+    await bot.add_cog(ExposureCountdown(bot))
