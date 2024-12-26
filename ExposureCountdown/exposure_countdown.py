360496d79325450ebd37c4c714616a468acbaf88
Jen <65942744+jenerative@users.noreply.github.com>
2024-12-26 12:49:03 -0600

    updated function

--- ExposureCountdown/exposure_countdown.py
+++ ExposureCountdown/exposure_countdown.py
@@ -59,5 +59,5 @@ class ExposureCountdown(commands.Cog):
     async def before_check_expiry(self):
         await self.bot.wait_until_ready()
 
-def setup(bot):
-    bot.add_cog(ExposureCountdown(bot))
+async def setup(bot):
+    await bot.add_cog(ExposureCountdown(bot))
