7fc922a5154e58797faca16f3d89cd2b4ade94da
Jen <65942744+jenerative@users.noreply.github.com>
2025-01-07 00:12:30 -0600

    trying to fix emoji only

--- MisoSoup/misosoup.py
+++ MisoSoup/misosoup.py
@@ -2,7 +2,6 @@ from redbot.core import commands, Config, checks, bank
 import discord
 from datetime import datetime, timedelta
 from discord.ext import tasks
-# import emoji
 import re
 
 class MisoSoup(commands.Cog):
@@ -264,7 +263,7 @@ class MisoSoup(commands.Cog):
     def is_emoji_only(self, content):
         custom_emoji_pattern = r'<a?:\w+:\d+>'
         unicode_emoji_pattern = r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U000024C2-\U0001F251]'
-        combined_pattern = f'^{custom_emoji_pattern}|{unicode_emoji_pattern}+$'
+        combined_pattern = f'({custom_emoji_pattern}|{unicode_emoji_pattern})+'
         return bool(re.fullmatch(combined_pattern, content))
 
 async def setup(bot):
